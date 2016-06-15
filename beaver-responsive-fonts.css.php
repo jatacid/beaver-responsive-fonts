dce9b7da34fcaa0770d4f109a355ee5954f052ba
Jay <jatacid@gmail.com>
2016-06-15 14:44:07 +1000

    1.0 commit

---
+++ beaver-responsive-fonts.css.php
@@ -0,0 +1,31 @@
+/* ###########Small and up########## */
+html {font-size: <?php echo $settings_min; ?>px !important;} /* minimum font size*/
+h1 {
+    font-size: <?php echo $settings_ratio['h1']; ?>rem !important;
+    line-height: calc(<?php echo $settings_ratio['h1']; ?>rem * 1.5) !important;
+}
+h2 {font-size: <?php echo $settings_ratio['h2']; ?>rem  !important;
+    line-height: calc(<?php echo $settings_ratio['h2']; ?>rem * 1.4) !important;
+}
+h3 {font-size: <?php echo $settings_ratio['h3']; ?>rem  !important;
+    line-height: calc(<?php echo $settings_ratio['h3']; ?>rem * 1.3) !important;
+}
+h4 {font-size: <?php echo $settings_ratio['h4']; ?>rem  !important;
+    line-height: calc(<?php echo $settings_ratio['h4']; ?>rem * 1.4) !important;
+}
+p, h5 {font-size: <?php echo $settings_ratio['h5']; ?>rem  !important;
+        line-height: calc(<?php echo $settings_ratio['h5']; ?>rem * 1.4) !important;
+}
+h6 {font-size: <?php echo $settings_ratio['h6']; ?>rem  !important;
+    line-height: calc(<?php echo $settings_ratio['h6']; ?>rem * 1.4) !important;
+}/* ###########Medium and up########### */
+@media screen and (min-width: 768px){
+html {font-size: 1.9vw !important;}
+h1 {font-size: <?php echo $settings_ratio['h1']; ?>rem !important;}
+}/* ###########Large and Up########### */
+@media screen and (min-width: 992px){
+html {font-size: 1.6vw !important;}
+}/* ##########Super Large And Up ############ */
+@media screen and (min-width: 1200px){
+html {font-size: <?php echo $settings_max; ?>px !important;}
+}
---
+++ beaver-responsive-fonts.php
@@ -0,0 +1,196 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+/*
+Plugin Name: Beaver Responsive Fonts
+Plugin URI: https://j7digital.com
+Description: Changes Beaver Builder Theme (or child theme) to have font settings scale on a perfect ratio instead of pixels.
+Author: J7Digital
+Version: 1.00
+Author URI: https://j7digital.com
+License: GPLv2 or later
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
+
+
+
+
+function change_customizer_defaults($wp_customize) {
+
+//$wp_customize->remove_section('fl-heading-font');
+//$wp_customize->remove_panel('fl-h1-font-size');
+
+$wp_customize->remove_control('fl-h1-font-size');
+$wp_customize->remove_control('fl-h2-font-size');
+$wp_customize->remove_control('fl-h3-font-size');
+$wp_customize->remove_control('fl-h4-font-size');
+$wp_customize->remove_control('fl-h5-font-size');
+$wp_customize->remove_control('fl-h6-font-size');
+
+$wp_customize->remove_control('fl-h1-line-height');
+$wp_customize->remove_control('fl-h2-line-height');
+$wp_customize->remove_control('fl-h3-line-height');
+$wp_customize->remove_control('fl-h4-line-height');
+$wp_customize->remove_control('fl-h5-line-height');
+$wp_customize->remove_control('fl-h6-line-height');
+
+$wp_customize->remove_control('fl-h1-letter-spacing');
+$wp_customize->remove_control('fl-h2-letter-spacing');
+$wp_customize->remove_control('fl-h3-letter-spacing');
+$wp_customize->remove_control('fl-h4-letter-spacing');
+$wp_customize->remove_control('fl-h5-letter-spacing');
+$wp_customize->remove_control('fl-h6-letter-spacing');
+
+
+// $mods = FLCustomizer::get_mods();
+// $vars = array();
+
+
+// Set a Minimum option (mobiles)
+// Set a Maximum option (super desktops)
+// Set Ratio? Golden Ratio
+
+
+// Add the djcustom Media Settings to the customizer.
+  $wp_customize->add_section( 'responsive-fonts', array(
+    'title'=> __( 'Responsive Typography Settings', 'fl-automator' ),
+    'description' => __( 'Overrides default Beaver Builder Typography with a more responsive equivalent.', 'fl-automator' ),
+    'priority'=> 130,
+    ) );
+
+  $wp_customize->add_setting('fl_responsive_font_minimum', array(
+  	'default' => '14'
+      )
+  );
+
+  $wp_customize->add_control('fl_responsive_font_minimum', array(
+    'label' => 'Responsive Minimum',
+    'description' => 'minimum paragraph font size (seen on mobiles)',
+    'section' => 'responsive-fonts',
+    'type' => 'number',
+    'input_attrs' => array(
+    	'min' => 10,
+    	'max' => 30),
+    )
+  );
+
+
+  $wp_customize->add_setting('fl_responsive_font_maximum', array(
+      'default' => '20'
+      )
+  );
+
+  $wp_customize->add_control('fl_responsive_font_maximum', array(
+    'label' => 'Responsive maximum',
+    'description' => 'maximum paragraph font size (seen on super desktops)',
+    'section' => 'responsive-fonts',
+    'type' => 'number',
+    'input_attrs' => array(
+    	'min' => 10,
+    	'max' => 30),
+    )
+  );
+
+
+
+
+  $wp_customize->add_setting('fl_responsive_font_ratio', array(
+      'default' => 'option-1'
+      )
+  );
+
+  $wp_customize->add_control('fl_responsive_font_ratio', array(
+    'label' => 'Ratio Type',
+    'description' => 'Select the ratio for font scaling',
+    'section' => 'responsive-fonts',
+    'type' => 'select',
+    'choices' => array(
+      'option-1' => 'Golden Ratio (Big & Beautiful)',
+      'option-2' => 'Perfect Fifth (Less dramatic)'),
+    )
+  );
+
+}
+add_action( 'customize_register', 'change_customizer_defaults', 99);
+
+
+
+
+
+// Customize mce editor font sizes
+if ( ! function_exists( 'wpex_mce_text_sizes' ) ) {
+    function wpex_mce_text_sizes( $initArray ){
+        $initArray['fontsize_formats'] = "0.25rem 0.5rem 0.75rem 1rem 1.25rem 1.5rem 1.75rem 2rem 2.25rem 2.5rem 2.75rem 3rem 3.25rem 3.5rem 3.75rem 4rem 4.25rem 4.5rem 4.75rem 5rem 5.25rem 5.5rem 5.75rem 6rem 8rem";
+        return $initArray;
+    }
+}
+add_filter( 'tiny_mce_before_init', 'wpex_mce_text_sizes' );
+
+
+
+
+
+function beaver_responsive_fonts_do_css(){
+
+ $settings =  FLCustomizer::get_mods();
+ $settings_min = $settings['fl_responsive_font_minimum'];
+    if ($settings_min !== false ){
+        $settings_min = $settings_min;
+    } else{
+    	$settings_min = '14';
+    }
+
+  $settings_max = $settings['fl_responsive_font_maximum'];
+    if ($settings_max !== false ){
+        $settings_max = $settings_max;
+    } else{
+    	$settings_max = '18';
+    }
+
+
+
+ $settings_ratio = $settings['fl_responsive_font_ratio'];
+    if ($settings_ratio !== false){
+        $settings_ratio = $settings_ratio;
+    } else{
+        $settings_ratio = 'option-1';
+    }
+
+if ($settings_ratio == 'option-1'){
+//Golden Ratio
+$settings_ratio = array(
+'h1' => '6.854',
+'h2' => '4.236',
+'h3' => '2.618',
+'h4' => '1.618',
+'h5' => '1.0',
+'h6' => '0.618',
+'p' => '1.0',
+);
+} else {
+//Perfect Fifth Ratio
+$settings_ratio = array(
+'h1' => '5.063',
+'h2' => '3.375',
+'h3' => '2.25',
+'h4' => '1.5',
+'h5' => '1.0',
+'h6' => '0.667',
+'p' => '1.0',
+);
+}
+
+echo '<style type="text/css">';
+include_once('beaver-responsive-fonts.css.php');
+echo '</style>';
+}
+add_action('fl_head', 'beaver_responsive_fonts_do_css', 99);
